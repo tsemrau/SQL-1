706012c743351c7ecc460ba7fe7c1b57957ef49d
tsemrau <tyler.semrau@kellogg.northwestern.edu>
2022-01-20 03:22:01 +0000

    changes class 3

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -4,3 +4,4 @@
 -- 2935
 
 
+SELECT stats.hits, count(*) from stats inner join players on stats.id = players.id WHERE first_name = "Barry" and last_name = "Bonds"
--- school-1-create.sql
+++ school-1-create.sql
@@ -4,4 +4,4 @@ CREATE TABLE students (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   first_name TEXT,
   last_name TEXT
-);
+DROP TABLE students (id INTEGER PRIMARY KEY)
